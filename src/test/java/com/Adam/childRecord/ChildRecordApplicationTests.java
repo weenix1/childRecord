c16ece65de4d466c7624418af2534be3d6c9d585
acu616 <hilary.ogalagu@haw-hamburg.de>
2023-02-10 23:18:12 +0100

    new setup project

---
+++ src/test/java/com/Adam/childRecord/ChildRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adam.childRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
