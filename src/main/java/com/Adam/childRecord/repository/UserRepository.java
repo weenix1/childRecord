a41f97540992c2808c64e585ad722435836e53c2
weenix1 <gallacero@gmail.com>
2023-02-13 10:55:50 +0100

    extract method implementated

--- src/main/java/com/Adam/childRecord/repository/UserRepository.java
+++ src/main/java/com/Adam/childRecord/repository/UserRepository.java
@@ -11,54 +11,53 @@ import java.util.List;
 
 public class UserRepository {
 
+    List<UserResponse> users = Arrays.asList(
+            new UserResponse("123456",
+                    "Doe",
+                    "John",
+                    LocalDate.of(1995, Month.JANUARY, 1),
+                    LocalTime.of(12, 0),
+                    "New York",
+                    "New Carolina",
+                    "Manhattan",
+                    "USA",
+                    Arrays.asList("game", "cool")
+            ),
+            new UserResponse("123457",
+                    "Moo",
+                    "Game",
+                    LocalDate.of(1995, Month.JANUARY, 1),
+                    LocalTime.of(12, 0),
+                    "New Jersey",
+                    "New York",
+                    "Manhattan",
+                    "USA",
+                    Arrays.asList("bad", "good")
+            ),
+            new UserResponse("123458",
+                    "Gooo",
+                    "Fish",
+                    LocalDate.of(1995, Month.JANUARY, 1),
+                    LocalTime.of(12, 0),
+                    "York",
+                    " York",
+                    "Manhattan",
+                    "USA",
+                    Arrays.asList("gone", "goat")
+            ),
+            new UserResponse("123459",
+                    "Dooo",
+                    "Frank",
+                    LocalDate.of(1995, Month.JANUARY, 1),
+                    LocalTime.of(12, 0),
+                    "Berlin",
+                    "Berlin",
+                    "Berlin",
+                    "Germany",
+                    Arrays.asList("Friendly", "Outgoing")
+            )
+    );
     public List<UserResponse> findAll(String tag){
-
-        List<UserResponse> users = Arrays.asList(
-                new UserResponse("123456",
-                        "Doe",
-                        "John",
-                        LocalDate.of(1995, Month.JANUARY, 1),
-                        LocalTime.of(12, 0),
-                        "New York",
-                        "New Carolina",
-                        "Manhattan",
-                        "USA",
-                        Arrays.asList("game", "cool")
-                ),
-                new UserResponse("123457",
-                        "Moo",
-                        "Game",
-                        LocalDate.of(1995, Month.JANUARY, 1),
-                        LocalTime.of(12, 0),
-                        "New Jersey",
-                        "New York",
-                        "Manhattan",
-                        "USA",
-                        Arrays.asList("bad", "good")
-                ),
-                new UserResponse("123458",
-                        "Gooo",
-                        "Fish",
-                        LocalDate.of(1995, Month.JANUARY, 1),
-                        LocalTime.of(12, 0),
-                        "York",
-                        " York",
-                        "Manhattan",
-                        "USA",
-                        Arrays.asList("gone", "goat")
-                ),
-                new UserResponse("123459",
-                        "Dooo",
-                        "Frank",
-                        LocalDate.of(1995, Month.JANUARY, 1),
-                        LocalTime.of(12, 0),
-                        "Berlin",
-                        "Berlin",
-                        "Berlin",
-                        "Germany",
-                        Arrays.asList("Friendly", "Outgoing")
-                )
-        );
 
         if(tag == null){
             return users;
@@ -67,15 +66,18 @@ public class UserRepository {
             List<UserResponse> filtered = new ArrayList<>();
 
             for(UserResponse user: users){
-
-                List<String> lowercaseTags = new ArrayList<>();
-                for(String t: user.getTags()) {
-                    lowercaseTags.add(t.toLowerCase());
-                }
-                if(lowercaseTags.contains(tag))
+                if(lowercaseTags(user).contains(tag))
                     filtered.add(user);
             }
             return filtered;
         }
+    }
+    private List<String> lowercaseTags(UserResponse user){
+        List<String> lowercaseTags = new ArrayList<>();
+        for(String t: user.getTags()) {
+            lowercaseTags.add(t.toLowerCase());
+        }
+        return lowercaseTags;
+
     }
 }
